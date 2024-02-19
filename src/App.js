3f8c6813738d540e0988755340dcca520a703565
VishalMX3 <f20191055@pilani.bits-pilani.ac.in>
2024-02-20 01:42:08 +0530

    updated navbar location

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import { NavBar, MovieList, Background } from "./components";
 
-import axios from 'axios';
-import './App.css';
+import axios from "axios";
+import "./App.css";
 
 const API_BASE_URL = "https://www.omdbapi.com";
 //api key = e3da3413
@@ -12,21 +12,48 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
 
   const search = async (e) => {
-    if (e.code === "Enter"){
+    if (e.code === "Enter") {
       setIsLoading(true);
-      const response = await axios.get (API_BASE_URL + "/?s=" + inputValue + "&apikey=aa660442");
-    setMovies(response.data.Search);
-    setIsLoading(false);
-    console.log(response.data.Search);
-  }};
+      const response = await axios.get(
+        API_BASE_URL + "/?s=" + inputValue + "&apikey=aa660442"
+      );
+      setMovies(response.data.Search);
+      setIsLoading(false);
+      console.log(response.data.Search);
+    }
+  };
+
+  // return (
+  //   <div className="main">
+  {
+    /* <NavBar
+        search={search}
+        setInputValue={setInputValue}
+        inputValue={inputValue}
+      /> */
+  }
+  {
+    /* {loading ? "Loading" : "Not Loading"}; */
+  }
 
   return (
     <div className="main">
-     <NavBar search = {search} setInputValue={setInputValue} inputValue={inputValue} />
-     {/* {loading ? "Loading" : "Not Loading"}; */}
-     
-     <MovieList isLoading = {isLoading} movieList = {movies}/>
-     <Background />
+      {movies.length > 0 ? (
+        <MovieList isLoading={isLoading} movieList={movies} />
+      ) : null}
+      {movies.length > 0 ? (
+        <MovieList isLoading={isLoading} movieList={movies} />
+      ) : null}
+      <Background />
+      <input
+        onChange={(e) => {
+          setInputValue(e.target.value);
+        }}
+        onKeyPress={search}
+        type="text"
+        placeholder="Search Movies"
+        className="search-bar"
+      />
     </div>
   );
 }
